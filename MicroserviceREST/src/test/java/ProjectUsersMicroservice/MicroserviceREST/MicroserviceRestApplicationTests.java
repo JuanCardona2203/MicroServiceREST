bdc3cef4128af0833772b7c48411cf6ddb2672b1
Juanesteban462 <juanesteban462@hotmail.com>
2022-05-23 17:51:48 -0500

    Files generated by platform of SpringBoot

---
+++ MicroserviceREST/src/test/java/ProjectUsersMicroservice/MicroserviceREST/MicroserviceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjectUsersMicroservice.MicroserviceREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
